7e9feeaec5543850e8b377ff8d74c053552e53b9
ToastinYou <zacharyhartt19@gmail.com>
2024-08-05 19:55:12 -0400

    Add project files.

---
+++ ToastyLifts/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ToastyLifts
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
