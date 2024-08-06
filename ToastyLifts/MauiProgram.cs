5db0f920be7530b271a818274a0daec59b87d863
ToastinYou <zacharyhartt19@gmail.com>
2024-08-05 21:45:24 -0400

    Register MainPage to IServiceCollection.

--- ToastyLifts/MauiProgram.cs
+++ ToastyLifts/MauiProgram.cs
@@ -2,40 +2,44 @@
 using ToastyLifts.Interfaces;
 using ToastyLifts.Services;
 using ToastyLifts.ViewModels;
+using ToastyLifts.Views;
 
 namespace ToastyLifts;
 
-    public static class MauiProgram
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
         MauiAppBuilder builder = MauiApp.CreateBuilder()
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
         builder.Services.Register();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
-
-        /// <summary>
-    /// Adds the application's services, view models, and views to the specified service collection.
-        /// </summary>
-        /// <param name="services">The application's <see cref="IServiceCollection"/>.</param>
-    private static void Register(this IServiceCollection services)
-        {
-            // Services
-            services.AddSingleton<IDatabaseService, DatabaseService>();
-
-            // View Models
-            services.AddTransient<MainViewModel>();
-        }
+        return builder.Build();
     }
+
+    /// <summary>
+    /// Adds the application's services, view models, and views to the specified service collection.
+    /// </summary>
+    /// <param name="services">The application's <see cref="IServiceCollection"/>.</param>
+    private static void Register(this IServiceCollection services)
+    {
+        // Services
+        services.AddSingleton<IDatabaseService, DatabaseService>();
+
+        // View Models
+        services.AddTransient<MainViewModel>();
+
+        // Views
+        services.AddTransient<MainPage>();
+    }
+}
